89f6d3aed90c3cea3d1f95e1c236d1b90fded8ad
badri narayanan sugavanam <bnarayanan@gmail.com>
2015-06-25 23:09:05 +1000

    Completed the function for fetching the trailer API and also with logging

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,9 +1,39 @@
 import media
 import fresh_tomatoes
-my_movies_list=[]
-toy_story=media.Movie("Toy Story","http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg","https://www.youtube.com/watch?v=Bj4gS1JQzjk","In a world where toys pretendto be lifeless in the presence of humans, Woody, a pullstring cowboy toy, is the leader of a group of toys owned by a boy named Andy Davis. With his family moving away one week before his birthday, Andy is given a week-early party to spend with his friends, while the toys stage a reconnaissance mission to discover Andy's new presents. Andy receives a spaceman action figure named Buzz Lightyear, whose impressive features see him replacing Woody as Andy's favorite toy. Woody becomes resentful, especially as Buzz also gets attention from the other toys. However, Buzz believes himself to be a real space ranger on a mission to return to his home planet while Woody tries to convince him that he is only a toy")
-san_anderas=media.Movie("San Andreas","http://upload.wikimedia.org/wikipedia/en/3/38/San_Andreas_poster.jpg","https://www.youtube.com/watch?v=23VflsU3kZE","San Andreas is a 2015 American 3D disaster film directed by Brad Peyton. The screenplay was written by Carlton Cuse, with Andre Fabrizio and Jeremy Passmore receiving a story credit. The film stars Dwayne Johnson, Carla Gugino, Alexandra Daddario, Hugo Johnstone-Burt, Art Parkinson, Ioan Gruffudd, Archie Panjabi and Paul Giamatti.")
-my_movies_list.append(toy_story)
-my_movies_list.append(san_anderas)
-fresh_tomatoes.open_movies_page(my_movies_list)
-#toy_story.show_trailer()
+
+#For Getting trailer a movie uses API for https://www.themoviedb.org/ using the lib@https://github.com/badrisugavanam/tmdbsimple
+import tmdbsimple as upcomingmovies
+
+#For all the movie info of a particular movie from https://www.themoviedb.org/ using the lib@https://github.com/badrisugavanam/themoviedb
+import tmdb
+import json
+import logging
+
+""" Builds my_movies_list fetching the latest information from https://www.themoviedb.org/  API 
+""" returns none
+def get_latest_upcoming_movie():
+    my_movies_list=[]
+    tmdb.configure('YOUR_API_KEY')
+    upcomingmovies.API_KEY = 'YOUR_API_KEY'
+    m1=upcomingmovies.Movies()
+    # get the latest upcoming movie
+    latest_movies=m1.upcoming()
+    logger.debug(latest_movies)
+    for c in m1.results:
+        movieinfo=tmdb.Movie(c['id'])
+        trailers=upcomingmovies.Movies(c['id'])
+        trailers.videos()
+        if(len(trailers.results) <> 0):
+            d=trailers.results[0]
+            logger.debug("trailer url"+"https://www.youtube.com/watch?v="+d['key'])
+            logger.debug("movie title"+movieinfo.get_title())
+            logger.debug("movie overview"+movieinfo.get_overview()) 
+            logger.debug("movie release date"+movieinfo.get_release_date())
+            movieresults=media.Movie(movieinfo.get_title(),movieinfo.get_poster('m'),"https://www.youtube.com/watch?v="+d['key'],movieinfo.get_overview(),movieinfo.get_release_date())
+            my_movies_list.append(movieresults)
+    fresh_tomatoes.open_movies_page(my_movies_list)
+
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename='fresh_tomatoes.log',level=logging.DEBUG)
+get_latest_upcoming_movie()
